2833d11de7b235c6b901c3f6ca9e718c11ce624a
James Wettenhall <james.wettenhall@gmail.com>
2019-11-19 13:39:06 +1100

    Remove some unused methods

--- mydata/utils/__init__.py
+++ mydata/utils/__init__.py
@@ -8,7 +8,6 @@ import sys
 import subprocess
 import traceback
 import unicodedata
-import webbrowser
 
 import appdirs
 import psutil
@@ -59,82 +58,6 @@ def bytes_to_human(num_bytes):
     return "%sB" % num_bytes
 
 
-def safe_str(err, input_enc=sys.getfilesystemencoding(), output_enc='utf-8'):
-    # pylint: disable=anomalous-unicode-escape-in-string
-    """
-    Safely return a string representation of an exception, possibly including
-    a user-defined filesystem path.
-
-    For an exception like this:
-
-        except Exception as err:
-
-    we can use str(err) to get a string representation of the error for
-    logging (or for displaying in a message dialog).
-
-    However in Python 2, Exception.__str__ can raise UnicodeDecodeError
-    if the exception contains non-ASCII characters.  In MyData, these
-    non-ASCII characters generally come from file or folder names which
-    MyData is scanning, so they are expected to be encoded using
-    sys.getfilesystemencoding().
-
-    Given that str(err) can raise UnicodeDecodeError, we could use
-    err.message, but it has been deprecated in some versions of Python 2
-    and has been removed in Python 3.  In most cases, it can be replaced
-    by err.args[0].  However in the case of IOError, the string
-    representation is a combination of the args, e.g.
-
-      "IOError: [Errno 5] foo: 'bar'"
-
-    for IOError(5, "foo", "bar").
-
-    SafeStr's default output encoding is utf-8, and before being encoded
-    as utf-8, the Unicode string is normalized.  For example, suppose a
-    filename contains the letter 'e' with an acute accent (é).  This could
-    be represented by a single Unicode code point: u'\u00E9', or as the
-    letter 'e' followed by the "combining acute accent", i.e. u'e\u0301'.
-    We can normalize these as follows:
-
-    >>> import unicodedata
-    >>> unicodedata.normalize('NFC', u'\u00E9')
-    u'\xe9'
-    >>> unicodedata.normalize('NFC', u'e\u0301')
-    u'\xe9'
-    >>> u'\xe9' == u'\u00E9'
-    True
-
-    """
-    try:
-        return str(err)
-    except UnicodeDecodeError:
-        input_enc = input_enc or 'utf-8'
-        if isinstance(err, (IOError, OSError)):
-            decoded = "%s: [Errno %s] %s: '%s'" \
-                % (type(err).__name__, err.errno,
-                   err.strerror.decode(input_enc),
-                   err.filename.decode(input_enc))
-        else:
-            decoded = err.args[0].decode(input_enc)
-    normalized = unicodedata.normalize('NFC', decoded)
-    return normalized.encode(output_enc)
-
-
-def compare(obj1, obj2):
-    """
-    Compare the two objects obj1 and obj2 and return an integer according
-    to the outcome. The return value is negative if obj1 < obj2, zero if
-    obj1 == obj2 and strictly positive if obj1 > obj2.
-    """
-    return (obj1 > obj2) - (obj1 < obj2)
-
-
-def open_url(url, new=0, autoraise=True):
-    """
-    Open URL in web browser or just check URL is accessible if running tests.
-    """
-    webbrowser.open(url, new, autoraise)
-
-
 def create_config_path_if_necessary():
     """
     Create path for saving MyData.cfg if it doesn't already exist.
@@ -152,36 +75,6 @@ def create_config_path_if_necessary():
     if not os.path.exists(appdir_path):
         os.makedirs(appdir_path)
     return appdir_path
-
-
-def initialize_trusted_certs_path():
-    """
-    Tell the requests module where to find the CA (Certificate Authority)
-    certificates bundled with MyData.
-    """
-    if hasattr(sys, "frozen"):
-        if sys.platform.startswith("darwin"):
-            cert_path = os.path.realpath(os.path.join(
-                os.path.dirname(sys.executable), '..', 'Resources'))
-        else:
-            cert_path = os.path.dirname(sys.executable)
-        os.environ['REQUESTS_CA_BUNDLE'] = \
-            os.path.join(cert_path, 'cacert.pem')
-
-
-def gnome_shell_is_running():
-    """
-    Check if the GNOME Shell desktop environment is running.
-    Helper function for CheckIfSystemTrayFunctionalityMissing.
-    """
-    for pid in psutil.pids():
-        try:
-            proc = psutil.Process(pid)
-            if 'gnome-shell' in proc.name():
-                return True
-        except psutil.NoSuchProcess:
-            pass
-    return False
 
 
 def mydata_install_location():
