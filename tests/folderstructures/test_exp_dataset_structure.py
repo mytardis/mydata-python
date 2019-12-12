963ba202abcd1787b953fb856a3139799b067d47
James Wettenhall <james.wettenhall@gmail.com>
2019-12-12 20:58:48 +1100

    Tidy up formatting

--- tests/folderstructures/test_exp_dataset_structure.py
+++ tests/folderstructures/test_exp_dataset_structure.py
@@ -11,6 +11,7 @@ from tests.mocks import (
     MOCK_INSTRUMENT_RESPONSE
 )
 
+
 def test_scan_exp_dataset_folders(set_exp_dataset_config):
     """Test ability to scan the Experiment / Dataset folder structure.
     """
@@ -31,11 +32,18 @@ def test_scan_exp_dataset_folders(set_exp_dataset_config):
         folders.append(folder)
 
     with requests_mock.Mocker() as mocker:
-        get_user_api_url = "%s/api/v1/user/?format=json&username=testfacility" % settings.general.mytardis_url
+        get_user_api_url = (
+            "%s/api/v1/user/?format=json&username=testfacility"
+        ) % settings.general.mytardis_url
         mocker.get(get_user_api_url, text=MOCK_USER_RESPONSE)
-        get_facility_api_url = "%s/api/v1/facility/?format=json" % settings.general.mytardis_url
+        get_facility_api_url = (
+            "%s/api/v1/facility/?format=json"
+        ) % settings.general.mytardis_url
         mocker.get(get_facility_api_url, text=MOCK_FACILITY_RESPONSE)
-        get_instrument_api_url = "%s/api/v1/instrument/?format=json&facility__id=1&name=Test%%20Instrument" % settings.general.mytardis_url
+        get_instrument_api_url = (
+            "%s/api/v1/instrument/?format=json&facility__id=1"
+            "&name=Test%%20Instrument"
+        ) % settings.general.mytardis_url
         mocker.get(get_instrument_api_url, text=MOCK_INSTRUMENT_RESPONSE)
 
         scan_folders(found_user, found_group, found_exp, found_dataset)
