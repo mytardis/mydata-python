7f742b0ba5718e69529615e80157a959158839a8
James Wettenhall <james.wettenhall@gmail.com>
2020-01-17 13:13:02 +1100

    Remove unhelpful comment and remove unneeded special case left over from MyData GUI's unit tests.

--- mydata/events/stop.py
+++ mydata/events/stop.py
@@ -11,14 +11,7 @@ from ..utils.exceptions import UserAborted
 def should_cancel_upload(upload):
     """Return True if the upload should be canceled
     """
-    if FLAGS.should_abort or upload.canceled:
-        return True
-
-    # This code would only run in tests where a scheduled progress query
-    # from one test attempts to run in a subsequent test, but the subsequent
-    # test doesn't create a folders controller instance.  (See the use of
-    # threading.Timer in mydata.utils.progress.)
-    return True
+    return FLAGS.should_abort or upload.canceled
 
 
 def raise_exception_if_user_aborted(set_status_message=None):
